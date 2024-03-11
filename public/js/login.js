d2954dcdeade23ededcb8c71711683559229a341
KabirIsrani <kabironly123@gmail.com>
2024-03-10 21:38:46 -0400

    Log In and Registration Backgroundadded

---
+++ public/js/login.js
@@ -0,0 +1,13 @@
+VANTA.GLOBE({
+  el: "body", 
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 0.50,
+  scaleMobile: 1.00,
+  color: 0x002699,
+  color2: 0x002699,
+  backgroundColor: 0xffffff
+})
--- public/js/register.js
+++ public/js/register.js
@@ -2,3 +2,17 @@ document.getElementById('brokerReferralBtn').addEventListener('click', function(
     document.getElementById('brokerReferralField').style.display = 'block';
     this.style.display = 'none'; 
 });
+
+VANTA.GLOBE({
+    el: "body", 
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 0.50,
+    scaleMobile: 1.00,
+    color: 0x002699,
+    color2: 0x002699,
+    backgroundColor: 0xffffff
+})
